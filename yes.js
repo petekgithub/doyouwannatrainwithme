16e1e627a78e7d02c81b6dac7c74fe0b4430d784
Petech <petekshamitbeyli@gmail.com>
2024-01-29 03:14:25 +0300

    back-btn

---
+++ yes.js
@@ -0,0 +1,6 @@
+// Event listener for the back button
+backBtn.addEventListener("click", (event) => {
+  // Navigate to the index.html page
+  event.preventDefault();
+  window.location.href = "/index.html";
+});

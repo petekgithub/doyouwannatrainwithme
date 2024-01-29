ce6ff844d6325f89a7323fe5decab69e1aa829f5
Petech <petekshamitbeyli@gmail.com>
2024-01-29 03:32:19 +0300

    update-3

--- yes.js
+++ yes.js
@@ -1,6 +1,9 @@
-// Event listener for the back button
-backBtn.addEventListener("click", (event) => {
-  // Navigate to the index.html page
-  event.preventDefault();
-  window.location.href = "/index.html";
+document.addEventListener("DOMContentLoaded", function () {
+  // Event listener for the back button
+  const backBtn = document.getElementById("backBtn");
+  backBtn.addEventListener("click", (event) => {
+    // Navigate to the index.html page
+    event.preventDefault();
+    window.location.href = "/index.html";
+  });
 });
